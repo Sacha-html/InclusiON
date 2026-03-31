393101606ce966fb9254fa047deee6caece8781c
Fernando <aparicio.fernando1986@gmail.com>
2026-03-31 19:59:05 -0300

    feat(Report):add query for get by id

---
+++ InclusiON.Application/UseCases/Reports/Queries/GetReportByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InclusiON.Application.UseCases.Reports.Queries
+{
+    public record GetReportByIdQuery(int ReportId);
+   
+}
