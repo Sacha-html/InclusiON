f2a1cca5f8b4d049b2bafd2f12c18e7eb91ea702
Fernando <aparicio.fernando1986@gmail.com>
2026-04-01 01:23:22 -0300

    fix: resolve review

--- InclusiON.Application/UseCases/Reports/Queries/GetReportByIdQuery.cs
+++ InclusiON.Application/UseCases/Reports/Queries/GetReportByIdQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InclusiON.Domain.Models;
+using InclusiON.DTOs.Responses.Reports;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,27 @@ using System.Threading.Tasks;
 
 namespace InclusiON.Application.UseCases.Reports.Queries
 {
-    public record GetReportByIdQuery(int ReportId);
+    public record GetReportByIdQuery(int ReportId)
+    {
+        internal static ReportResponse MapToResponse(Report report)
+        {
+            return new ReportResponse
+            {
+                Id = report.Id,
+                Title = report.Title,
+                Content = report.Content,
+                ReportDate = report.ReportDate,
+                PersonId = report.PersonId,
+                ProfessionalId = report.ProfessionalId,
+                ReportTypeId = report.ReportTypeId,
+                AchievedGoals = report.AchievedGoals,
+                AreasToReinforce = report.AreasToReinforce,
+                FutureRecommendations = report.FutureRecommendations,
+                NextObjectives = report.NextObjectives,
+                IsActive = report.IsActive,
+            };
+        }
+    }
+        
    
 }
