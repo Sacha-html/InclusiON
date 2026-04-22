3cfad7bbe647f526c1641e8fa1d3821e1ad06b44
mwlk <mirkoivowlk@gmail.com>
2026-04-21 22:58:32 -0300

    chore: add response classes

--- InclusiON.DTOs/Responses/Admin/ResetPasswordResultResponse.cs
+++ InclusiON.DTOs/Responses/Admin/ResetPasswordResultResponse.cs
@@ -2,7 +2,6 @@ namespace InclusiON.DTOs.Responses.Admin
 {
     public class ResetPasswordResultResponse
     {
-        public string TemporaryPassword { get; set; } = string.Empty;
         public string UserEmail { get; set; } = string.Empty;
     }
 }
