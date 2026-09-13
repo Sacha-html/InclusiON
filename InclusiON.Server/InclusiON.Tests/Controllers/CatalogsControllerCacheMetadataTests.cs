dfb8e3b8a3b1d537035070a729fb6437a17c37e4
Fernando <aparicio.fernando1986@gmail.com>
2026-09-13 01:18:51 -0300

    fix: actualizar grilla de tipos de discapacidad

--- InclusiON.Server/InclusiON.Tests/Controllers/CatalogsControllerCacheMetadataTests.cs
+++ InclusiON.Server/InclusiON.Tests/Controllers/CatalogsControllerCacheMetadataTests.cs
@@ -9,6 +9,20 @@ namespace InclusiON.Tests.Controllers;
 public class CatalogsControllerCacheMetadataTests
 {
     [Fact]
+    public void GetDisabilityTypes_DisablesHttpAndOutputCaching()
+    {
+        var method = typeof(CatalogsController).GetMethod(nameof(CatalogsController.GetDisabilityTypes));
+        var responseCache = method!.GetCustomAttributes(typeof(ResponseCacheAttribute), inherit: true)
+            .Cast<ResponseCacheAttribute>().Single();
+        var outputCache = method.GetCustomAttributes(typeof(OutputCacheAttribute), inherit: true)
+            .Cast<OutputCacheAttribute>().Single();
+
+        responseCache.NoStore.Should().BeTrue();
+        responseCache.Location.Should().Be(ResponseCacheLocation.None);
+        outputCache.NoStore.Should().BeTrue();
+    }
+
+    [Fact]
     public void GetSpecialties_DisablesHttpResponseCaching()
     {
         var controllerResponseCache = typeof(CatalogsController).GetCustomAttributes(typeof(ResponseCacheAttribute), inherit: true)
