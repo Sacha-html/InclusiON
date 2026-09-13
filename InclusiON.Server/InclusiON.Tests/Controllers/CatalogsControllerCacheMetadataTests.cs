ea80c40fe6f7219180d3b1e3d58812de1c880f95
Fernando <aparicio.fernando1986@gmail.com>
2026-09-12 23:00:04 -0300

    fix: refrescar grilla de áreas de habilidad

--- InclusiON.Server/InclusiON.Tests/Controllers/CatalogsControllerCacheMetadataTests.cs
+++ InclusiON.Server/InclusiON.Tests/Controllers/CatalogsControllerCacheMetadataTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 using InclusiON.Api.Controllers;
 using Xunit;
 
@@ -21,5 +22,25 @@ public class CatalogsControllerCacheMetadataTests
         controllerResponseCache.Duration.Should().Be(300);
         responseCache.NoStore.Should().BeTrue();
         responseCache.Location.Should().Be(ResponseCacheLocation.None);
+    }
+
+    [Fact]
+    public void GetSkillAreas_DisablesHttpAndOutputCaching()
+    {
+        var controllerResponseCache = typeof(CatalogsController).GetCustomAttributes(typeof(ResponseCacheAttribute), inherit: true)
+            .Cast<ResponseCacheAttribute>()
+            .Single();
+        var method = typeof(CatalogsController).GetMethod(nameof(CatalogsController.GetSkillAreas));
+        var responseCache = method!.GetCustomAttributes(typeof(ResponseCacheAttribute), inherit: true)
+            .Cast<ResponseCacheAttribute>()
+            .Single();
+        var outputCache = method.GetCustomAttributes(typeof(OutputCacheAttribute), inherit: true)
+            .Cast<OutputCacheAttribute>()
+            .Single();
+
+        controllerResponseCache.Duration.Should().Be(300);
+        responseCache.NoStore.Should().BeTrue();
+        responseCache.Location.Should().Be(ResponseCacheLocation.None);
+        outputCache.NoStore.Should().BeTrue();
     }
 }
